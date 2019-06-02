9c158b7cc70c63dbfbce7839b80a903e1503e7f6
manjit0815 <manjit0815@gmail.com>
2019-06-01 23:47:28 -0400

    final 3

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -15,7 +15,7 @@ test -d ~/Pictures || mkdir ~/Pictures
 test -f ~/Pictures/pics.tgz || wget -q -O ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
 
 # unpack the downloaded zipfile if it is there, then delete the local copy of the zipfile
-test -f ~/Pictures/pics.tgz && tar -xvzf ~/Pictures ~/Pictures/pics.tgz && rm ~/Pictures/pics.tgz
+test -f ~/Pictures/pics.tgz && tar -xvzf ~/Pictures/pics.tgz || rm ~/Pictures/pics.tgz
 
 # Make a report on what we have in the Pictures directory
 test -d ~/Pictures && cat <<EOF
